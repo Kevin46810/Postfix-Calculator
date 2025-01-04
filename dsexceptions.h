1f661728ddbe53ca4ed1b11db3ca567446e20615
Kevin46810 <kbind101@gmail.com>
2025-01-04 01:48:05 -0500

    Create dsexceptions.h

---
+++ dsexceptions.h
@@ -0,0 +1,7 @@
+        #ifndef _DSEXCEPTIONS_H_
+        #define _DSEXCEPTIONS_H_
+
+        class Underflow { };
+        class Overflow  { };
+        class DataError { };
+        class DivisionByZero { };
